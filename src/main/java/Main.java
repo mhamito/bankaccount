72c2e2cc76b1b47cf4e37fb68770aff920b5e12c
saidhamitouche <saidh.hamitouche@gmail.com>
2020-10-14 18:29:09 +0200

    Saidh : print account statements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 public class Main {
 
     public static void main(String[] arg) {
+
+        Account account = new Account();
+        PrintStatements printStatements = new PrintStatements();
+
+
+        account.deposit(new Amount(1000));
+        account.deposit(new Amount(2500));
+        account.withdrawal(new Amount(1000.0));
+
+
+        printStatements.print(account.getAccountStatements());
     }
 }
---
+++ src/main/java/PrintStatements.java
@@ -0,0 +1,15 @@
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class PrintStatements {
+
+    public PrintStatements(){}
+
+    public void print(List<AccountStatement> accountStatements) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        System.out.println("Operation |  Date of operation  |  Amount  |  Balance  |");
+        accountStatements.forEach(accountStatement -> {
+            System.out.println(accountStatement.getOperationType() + " | " + accountStatement.getDate() + " | " + decimalFormat.format(accountStatement.getAmount()) + " € | " + decimalFormat.format(accountStatement.getBalance()) + " €");
+        });
+    }
+}
