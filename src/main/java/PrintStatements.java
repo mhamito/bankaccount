826ab05dd65ca07753718582b25048c64295aa6b
saidhamitouche <saidh.hamitouche@gmail.com>
2020-10-14 18:35:08 +0200

    Saidh : reverse list of my statements when printing my account statements

--- src/main/java/PrintStatements.java
+++ src/main/java/PrintStatements.java
@@ -1,4 +1,5 @@
 import java.text.DecimalFormat;
+import java.util.Collections;
 import java.util.List;
 
 public class PrintStatements {
@@ -8,6 +9,7 @@ public class PrintStatements {
     public void print(List<AccountStatement> accountStatements) {
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
         System.out.println("Operation |  Date of operation  |  Amount  |  Balance  |");
+        Collections.reverse(accountStatements);
         accountStatements.forEach(accountStatement -> {
             System.out.println(accountStatement.getOperationType() + " | " + accountStatement.getDate() + " | " + decimalFormat.format(accountStatement.getAmount()) + " € | " + decimalFormat.format(accountStatement.getBalance()) + " €");
         });
